d74ccd50fe4b437fd552488a582d77daac2aede1
Christopher Newell <chris@ineedhelp.com.au>
2017-11-30 13:36:48 +1100

    Annotation Scripts
    Create, Export and Import Annotations

---
+++ Annotations/Annotation-Create/create-annotations.ps1
@@ -0,0 +1,28 @@
+New-CustomAttribute -Name "Application Team" -TargetType VirtualMachine
+New-CustomAttribute -Name "Backup Requirements" -TargetType VirtualMachine
+New-CustomAttribute -Name "Build Profile" -TargetType VirtualMachine
+New-CustomAttribute -Name "CAPPLAN-ID" -TargetType VirtualMachine
+New-CustomAttribute -Name "Created" -TargetType VirtualMachine
+New-CustomAttribute -Name "Domain" -TargetType VirtualMachine
+New-CustomAttribute -Name "Email Contact" -TargetType VirtualMachine
+New-CustomAttribute -Name "Managed By" -TargetType VirtualMachine
+New-CustomAttribute -Name "NIC1 DNS Zone" -TargetType VirtualMachine
+New-CustomAttribute -Name "NIC1 Gateway" -TargetType VirtualMachine
+New-CustomAttribute -Name "NIC1 IP" -TargetType VirtualMachine
+New-CustomAttribute -Name "NIC1 MAC" -TargetType VirtualMachine
+New-CustomAttribute -Name "NIC1 Subnet" -TargetType VirtualMachine
+New-CustomAttribute -Name "NIC2 DNS Zone" -TargetType VirtualMachine
+New-CustomAttribute -Name "NIC2 Gateway" -TargetType VirtualMachine
+New-CustomAttribute -Name "NIC2 IP" -TargetType VirtualMachine
+New-CustomAttribute -Name "NIC2 MAC" -TargetType VirtualMachine
+New-CustomAttribute -Name "NIC2 Subnet" -TargetType VirtualMachine
+New-CustomAttribute -Name "No Backup Reason" -TargetType VirtualMachine
+New-CustomAttribute -Name "OS Build Status" -TargetType VirtualMachine
+New-CustomAttribute -Name "Operating System" -TargetType VirtualMachine
+New-CustomAttribute -Name "Project" -TargetType VirtualMachine
+New-CustomAttribute -Name "Security Zone" -TargetType VirtualMachine
+New-CustomAttribute -Name "Service" -TargetType VirtualMachine
+New-CustomAttribute -Name "System" -TargetType VirtualMachine
+New-CustomAttribute -Name "Vendor" -TargetType VirtualMachine
+
+New-CustomAttribute -Name "Blade Chassis" -TargetType VMHost
